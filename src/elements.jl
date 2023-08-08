c0bd95b89f725907038440bf5afbbf67a6eea2a8
PierreBarrat <p.barrat@live.fr>
2023-08-08 11:38:43 +0200

    tmp: not working

--- src/elements.jl
+++ src/elements.jl
@@ -30,19 +30,44 @@ function Branch(parent, child, color::AbstractVector, len)
 end
 
 
-"""
-    ancestor(x::TreeNode)
+####################################################################
+############################### Node{K} ##########################
+####################################################################
 
-Return the (unique) ancestor of `x` and the branch color, as a tuple
+#=
+Docstrings of joint methods
+=#
+
+"""
+    children(x::Node)
+
+Return an iterator over the children of `x`.
+Elements are tuples of the form `(child, branch_color)`.
+"""
+function children end
+
+"""
+    ancestor(x::Node, color)
+Return the ancestor of `x` for `color`.
 If `x` is a root (*i.e.* no ancestor) return `nothing` instead of the ancestor.
+If there is no unambiguous ancestor for `color`, throw an error.
 """
 function ancestor end
 
 """
-    find_ancestor(child::Node{K}, parent::Node{K})
+    ancestors(x::Node)
+
+Return an iterator over the ancestors of x.
+Elements are tuples of the form `(ancestor, branch_color)`.
+"""
+function ancestors end
+
+"""
+    find_ancestor(child::Node, parent::Node)
 
 Return the branch above `child` leading to `parent`, and an index if relevant.
 Output format: `(idx, branch)` where both `idx` and `branch` can be `nothing`.
+If `idx` is an integer, then `child.up_branch[i] == branch`.
 """
 function find_ancestor end
 
@@ -54,20 +79,20 @@ If the branch up from x does not have all colors of x, then x is a root.
 function isroot end
 
 """
-    hasancestor(child, parent)
-    hasancestor(child, parent, color)
+    hasancestor(child::Node, parent::Node[, color])
 
 Does `child` has `parent` as an ancestor?
 """
 function hasancestor end
 
 """
-    _add_ancestor!(child::Element, branch)
-    _add_child!(parent::Element, branch)
-    _remove_ancestor!(child::Element, branch)
-    _remove_child!(parent::Element, branch)
+    _add_ancestor!(child::Node, branch)
+    _add_child!(parent::Node, branch)
+    _remove_ancestor!(child::Node, branch)
+    _remove_child!(parent::Node, branch)
 
-Not to use directly. Add a branch down/up an element. Perform basic checks.
+Not to use directly. Add or remove a branch down/up an node.
+Perform basic checks based on the node.
 """
 function _add_ancestor! end
 function _add_child! end
@@ -76,12 +101,12 @@ function _remove_child! end
 
 #=
 To implement
-- ancestor(node[, color])
-- child(node)
-- hasancestor(child::SpecElement, parent::Element)
-- haschile(child::SpecElement, parent::Element)
-- find_ancestor(child::SpecElement, parent::Element) --> (idx, branch_to_ancestor)
-- _add_ancestor!(node::SpecElement, branch)
+- ancestor(node, color) / ancestors(node)
+- children(node)
+- hasancestor(child::SpecNode, parent::Node)
+- haschild(child::SpecNode, parent::Node)
+- find_ancestor(child::SpecNode, parent::Node) --> (idx, branch_to_ancestor)
+- _add_ancestor!(node::SpecNode, branch)
 - _add_child!(node::, branch)
 - _remove_ancestor!(node::, branch)
 - _remove_child!(node::, branch)
@@ -94,7 +119,11 @@ To implement
 ####################################################################
 ############################## TreeNode{K} #########################
 ####################################################################
+"""
+    TreeNode{K} <: Node{K}
 
+Node with one or no ancestor, and any number of children.
+"""
 mutable struct TreeNode{K} <: Node{K}
     id::String
     label::String
@@ -128,14 +157,32 @@ function TreeNode{K}(;
     branch_length = missing,
 ) where K
     node = TreeNode{K}(id, label, nothing, [], color)
-    node.up_branch = nothing
     return node
 end
 
 Base.:(==)(x::TreeNode{K}, y::TreeNode{K}) where K = (x.id == y.id)
 Base.hash(x::TreeNode, h::UInt64) = hash(x.id, h)
 
+"""
+    child(x::TreeNode, i)
 
+Return the ith child of x along with the branch color: `(child, color)`.
+"""
+child(x::TreeNode, i) = (x.down_branches[i].child, x.down_branches[i].color)
+children(x::TreeNode) = Iterators.map(b -> (b.child, b.color), x.down_branches)
+
+"""
+    haschild(parent, child)
+"""
+haschild(parent::TreeNode{K}, child::Node{K}) where K = in(child, children(parent))
+
+"""
+    ancestor(x::TreeNode)
+
+Return the ancestor of `x` along with the color of the branch leading to it, as
+a tuple of the form `(ancestor, color)`.
+If `x` is a root, return `(nothing, x.color)`.
+"""
 function ancestor(x::TreeNode)
     return if isnothing(x.up_branch)
         (nothing, x.color)
@@ -167,8 +214,20 @@ function ancestor(x::TreeNode, color)
     end
 end
 ancestor(x::TreeNode{K}, color::Vararg{<:Integer}) where K = ancestor(x, Color{K}(color...))
-ancestors(x::TreeNode, color) = (ancestor(x, color), )
 ancestors(x::TreeNode) = (ancestor(x),)
+
+hasancestor(child::TreeNode{K}, parent::Node{K}) where K = (ancestor(child)[1] == parent)
+function hasancestor(child::TreeNode{K}, parent::Node{K}, color) where K
+    return ancestor(child, Color{K}(color)) == parent
+end
+function hasancestor(child, parent::Node{K}, color...) where K
+    return hasancestor(child, parent, Color{K}(color...))
+end
+
+function find_ancestor(child::TreeNode{K}, parent::Node{K}) where K
+    hasancestor(child, parent) ? (nothing, child.up_branch) : (nothing, nothing)
+end
+
 
 branch_length(x::TreeNode) = x.up_branch.len
 function branch_length(x::TreeNode, color)
@@ -179,9 +238,6 @@ function branch_length(x::TreeNode, color::Vararg{<:Integer})
     @assert hascolor(x, color...) "ColorError" x color
     return branch_length(x)
 end
-
-children(x::TreeNode) = Iterators.map(b -> b.child, x.down_branches)
-child(x::TreeNode, i) = x.down_branches[i].child
 
 isleaf(x::TreeNode) = isempty(x.down_branches)
 issingleton(x::TreeNode) = (length(x.down_branches) == 1)
@@ -220,24 +276,6 @@ function check_node(x::TreeNode{K}) where K
     return true
 end
 
-hasancestor(child::TreeNode{K}, parent::Node{K}) where K = (ancestor(child)[1] == parent)
-function hasancestor(child::TreeNode{K}, parent::Node{K}, color) where K
-    return ancestor(child, Color{K}(color)) == parent
-end
-function hasancestor(child, parent::Node{K}, color...) where K
-    hasancestor(child, parent, Color{K}(color...))
-end
-
-"""
-    haschild(parent, child)
-"""
-haschild(parent::TreeNode{K}, child::Node{K}) where K = in(child, children(parent))
-
-
-function find_ancestor(child::TreeNode{K}, parent::Node{K}) where K
-    hasancestor(child, parent) ? (nothing, child.up_branch) : (nothing, nothing)
-end
-
 
 function _add_ancestor!(child::TreeNode, branch)
     isnothing(child.up_branch) || throw(ErrorException("Node $child already has an ancestor"))
@@ -262,14 +300,86 @@ end
 ####################################################################
 ############################## HybridNode{K} #########################
 ####################################################################
-# mutable struct HydridNode{K} <: Node{K}
-#     id::String
-#     label::String
-#     up_branches::Vector{Branch{K}}
-#     down_branch::Union{Nothing, Branch{K}}
-#     color::Color{K}
+"""
+    HybridNode{K} <: Node{K}
 
-#     function HybridNode{K}(id, label, up_branches, down_branch, color::Color{K}) where K
-#         return new(id, label, up_branches, down_branch, color)
-#     end
-# end
+Node with at least 2 and at most `K` ancestors, and exactly one child.
+
+!!! note
+For ease of implementation, a `HybridNode` can have no branch going down, *i.e.* can have no child. This should be an exceptional situation that only arises during initialization of the node. Such a node will throw an error when `check_node` is called.
+!!!
+"""
+mutable struct HydridNode{K} <: Node{K}
+    id::String
+    label::String
+    up_branches::Vector{Branch{K}}
+    down_branch::Union{Nothing, Branch{K}}
+    color::Color{K}
+
+    function HybridNode{K}(id, label, up_branches, down_branch, color::Color{K}) where K
+        return new(id, label, up_branches, down_branch, color)
+    end
+end
+function HybridNode{K}(id, label, up_branches, down_branch, color::AbstractVector) where K
+    return HybridNode{K}(id, label, up_branches, down_branch, Color{K}(color))
+end
+"""
+    HybridNode{K}(;
+        label = "TreeNode",
+        color = ones(MVector{K, Bool}),
+        branch_length = missing,
+    ) where K
+
+Construct an unlinked `HybridNode`: connected upwards to a root and downwards to nothing.
+`color` should be in the onehot format (see `SRG.index_to_onehot`).
+"""
+function HybridNode{K}(;
+    id = randstring(id_length),
+    label = "HybridNode",
+    color = Color{K}(ones(Bool, K)),
+    branch_length = missing,
+) where K
+    return HybridNode{K}(id, label, [], nothing, color)
+end
+
+
+Base.:(==)(x::HybridNode{K}, y::HybridNode{K}) where K = (x.id == y.id)
+Base.hash(x::HybridNode, h::UInt64) = hash(x.id, h)
+
+function children(x::HybridNode)
+end
+
+function haschild(parent::HybridNode{K}, child::Node{K}) where K
+end
+
+function ancestors(x::HybridNode)
+end
+
+function ancestor(x::HybridNode, color)
+end
+ancestor(x::HybridNode{K}, color::Vararg{<:Integer}) where K = ancestor(x, Color{K}(color...))
+
+
+function hasancestor(child::HybridNode{K}, parent::Node{K}) where K
+end
+function hasancestor(child::HybridNode{K}, parent::Node{K}, color)
+end
+function hasancestor(child::HybridNode{K}, parent::Node{K}, color...) where K
+    return hasancestor(child, parent, Color{K}(color...))
+end
+
+function find_ancestor(child::HybridNode{K}, parent::Node{K}) where K
+end
+
+function branch_length(x::HybridNode, color)
+end
+
+isroot(x::HybridNode) = false
+isleaf(x::HybridNode) = false
+function issingleton(x::HybridNode)
+end
+
+function check_node(x::HybridNode{K}) where K
+end
+
+
